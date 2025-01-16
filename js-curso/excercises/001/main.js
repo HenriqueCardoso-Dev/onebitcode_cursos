32c31fb8de3d3d7d89c64f13d791746ec0eb298b
Carlos Henrique Ramos Cardoso <carloshenriqueramoscardoso@gmail.com>
2025-01-16 18:02:14 -0300

    excercise

--- js-curso/excercises/001/main.js
+++ js-curso/excercises/001/main.js
@@ -4,9 +4,20 @@ function runApp() {
     console.log('starting...');
 
     try {
-        console.log(`moduleFunctions.createFile("conteúdo inicial do arquivo './text-file.txt' criado com fs do node.js");`);        
+        console.log(`moduleFunctions.createFile("conteúdo inicial do arquivo './text-file.txt' criado com fs do node.js");`);
         moduleFunctions.createFile("conteúdo inicial do arquivo './text-file.txt' criado com fs do node.js");
-        console.log(`readFile() => `+moduleFunctions.readFile());
+        moduleFunctions.readFile();
+
+        setTimeout(() => {
+            console.log(`moduleFunctions.createFile("Conteúdo modificado")`);
+            moduleFunctions.createFile("Conteúdo modificado");
+            moduleFunctions.readFile();
+        }, 1000);
+
+        setTimeout(() => {
+            moduleFunctions.deleteFile();
+        }, 2000);
+
     } catch (err) {
         console.log(err);
     } 
